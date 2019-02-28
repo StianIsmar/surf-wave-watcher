47c64d391ef253ecf944c53cd6af14cfab10899e
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-02-28 18:26:20 +0100

    Gave button some styling

--- wave-react/src/components/WaveData.jsx
+++ wave-react/src/components/WaveData.jsx
@@ -2,17 +2,16 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import { fetchWaveData } from '../actions/WaveActions'
 import { Button } from "react-bootstrap"
+import './WaveData.css'
 
 class WaveData extends Component {
     componentDidMount() {
         console.log("Fire getWaveData!")
       }
-    
-
     render() {
         return (
-            <div>
-            <Button onClick={() =>
+            <div className="btn">
+            <Button variant="primary" size="lg" onClick={() =>
             this.props.getWaveData()
             }>
                 Press to get latest forecast
