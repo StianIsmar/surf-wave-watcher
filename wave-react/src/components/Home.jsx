0669794b1ba758a488db15ce235e0677e3e4737e
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-03-18 09:34:22 +0100

    Move the Jumbatron up

--- wave-react/src/components/Home.jsx
+++ wave-react/src/components/Home.jsx
@@ -5,24 +5,32 @@ import './Home.css'
 import { zip } from 'rxjs';
 import WaveData from './WaveData'
 import WaveCard from './WaveCard';
+import {MDBIcon } from 'mdbreact';
+
+
+
+
 export default class Home extends Component{
     render(){
         return(
             <div>
                  <div className = "top-part">
-                 <p> <div className= "h22">Scroll to see what the waves are like at Hustadvika
+                 <p> <div className= "h22">
                  </div>
                  </p>
                 </div>
             <Container className = "container">
-            <WaveCard />
-                <Jumbotron>
-                    <h2>Welcome to wave watcher</h2>
-                    <Link to ="More pictures">
-                    <p></p>
-                        <Button  bsStyle="primary"> More images</Button>
-                    </Link>
+            <Jumbotron>
+                    {/*<h2>Welcome to wave watcher</h2>*/}
+                    <h4>Scroll down to see what the waves are like at Hustadvika right now</h4>
+                    <MDBIcon icon="angle-double-down" />
+                    {/*<Link to ="More pictures">*/}
+                    {/*<p></p>*/}
+                        {/*<Button  bsStyle="primary"> More images</Button>
+                    {/*</Link>*/}
                 </Jumbotron>
+            <WaveCard />
+               
 
                 <Image src="assets/surfer_header.jpg" className = "top-image1"  />
 
